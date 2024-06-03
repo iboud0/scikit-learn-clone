31b6b2aa59c440dbf2efd8e169ea90f425725312
unknown <ilyasboudhaine1@gmail.com>
2024-06-03 02:10:58 +0100

    setup3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sklearn_clone',
-    version='0.1.0',
+    version='0.1.1',
     description='A custom implementation of various machine learning algorithms and utilities inspired by Scikit-Learn',
     author='Ilyas Boudhaine & Zineb Abercha',
     author_email='ilyasboudhaine1@gmail.com, zineb03abercha@gmail.com',
